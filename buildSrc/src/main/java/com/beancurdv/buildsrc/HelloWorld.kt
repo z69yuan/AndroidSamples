e7a5b15522f605cea6267af7113feca142444d89
beancurdv <z69yuan@163.com>
2023-08-25 18:12:28 +0800

    ASM-buildSrc temp

---
+++ buildSrc/src/main/java/com/beancurdv/buildsrc/HelloWorld.kt
@@ -0,0 +1,13 @@
+package com.beancurdv.buildsrc
+
+/**
+ *
+ *
+ * @author  on 2023/8/25
+ */
+open class HelloWorld {
+}
+
+class X : HelloWorld() {
+    val m = listOf<String>("a","b")
+}
--- buildSrc/src/main/java/com/beancurdv/buildsrc/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.beancurdv.buildsrc
-
-class MyClass {
-}
